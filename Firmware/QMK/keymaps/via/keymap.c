ff6e09e01e8aa5074ae6055f4c4a20879554ceee
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2025-03-14 01:08:54 -0400

    feat: add via.json

--- Firmware/QMK/keymaps/via/keymap.c
+++ Firmware/QMK/keymaps/via/keymap.c
@@ -62,3 +62,17 @@ bool oled_task_user(void) {
     return false;
 }
 #endif
+
+#ifdef ENCODER_MAP_ENABLE
+/*
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [0] = { ENCODER_CCW_CW(MS_WHLU, MS_WHLD),  ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
+    [1] = { ENCODER_CCW_CW(UG_HUED, UG_HUEU),  ENCODER_CCW_CW(UG_SATD, UG_SATU)  },
+    [2] = { ENCODER_CCW_CW(UG_VALD, UG_VALU),  ENCODER_CCW_CW(UG_SPDD, UG_SPDU)  },
+    [3] = { ENCODER_CCW_CW(UG_PREV, UG_NEXT),  ENCODER_CCW_CW(KC_RIGHT, KC_LEFT) },
+};
+*/
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+  [0] = { ENCODER_CCW_CW(KC_VOLU, KC_VOLD)},
+};
+#endif
